74db21f11397c36060d27f9cace2ece939b0dd6c
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-11-14 13:27:02 +0700

    debug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+//import React from "react";
 import {
   MDBFooter,
   MDBContainer,
-  MDBCol,
-  MDBRow,
+  //  MDBCol,
+  //  MDBRow,
   MDBIcon,
   MDBBtn,
 } from "mdb-react-ui-kit";
